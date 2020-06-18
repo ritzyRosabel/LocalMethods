000e35e465372c12aafa6e982281e0f10cdcccc7
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-06-18 23:52:08 +0100

    My confession

--- Local Methods/Program.cs
+++ Local Methods/Program.cs
@@ -8,6 +8,7 @@ namespace Local_Methods
 {
     class Program
     {
+        //If this code works, then it was writen by Rosabel Olugbenga. If it doesn't, then i can only say one thing  "IT IS WHAT IT IS".
         static void Main(string[] args)
         {
             LocalMethodExample localMethod = new LocalMethodExample();
